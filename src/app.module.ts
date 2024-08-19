8762eb9eaa83a4a729f91a282e4169018874a7ca
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-08-19 08:26:05 +0330

    Feat : use ConfigModule and TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,12 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { DatabaseConfigs } from './configs/database.config';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [
+        ConfigModule.forRoot({ envFilePath: '.env', isGlobal:true }),
+        TypeOrmModule.forRoot(DatabaseConfigs()),
+    ],
+})
 export class AppModule {}
