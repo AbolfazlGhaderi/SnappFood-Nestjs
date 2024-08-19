f5f13e3feb7422131752502cc9e18bf2916e2a41
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-08-19 08:32:38 +0330

    Feat : add User Entity

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { BaseEntity } from '../common/abstracts/base.entity';
+import { Column, DeleteDateColumn, Entity, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UserEntity extends BaseEntity
+{
+    @Column({ nullable: false })
+    first_name: string;
+    @Column({ nullable: true })
+    last_name: string;
+    @Column({ nullable: false, unique: true })
+    mobile: string;
+    @Column({ nullable: false, unique: true })
+    email: string;
+    @Column({ nullable: false, unique: true })
+    invite_code: string;
+    @Column({ default: 0 })
+    score: number;
+    @Column({ nullable:true })
+    agentId: number; // maybe not needed
+    @UpdateDateColumn({ type: 'timestamp with time zone' })
+    update_at: Date;
+    @DeleteDateColumn({ type: 'timestamp with time zone' })
+    delete_at: Date;
+}
