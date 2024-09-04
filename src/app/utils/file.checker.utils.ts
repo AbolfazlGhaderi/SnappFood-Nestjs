013c60ac03f6da49db3f800805072bcccbe73725
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-09-04 21:13:37 +0330

    Feat : Add File Checker class for check file format

---
+++ src/app/utils/file.checker.utils.ts
@@ -0,0 +1,29 @@
+import { extname } from 'node:path';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { ValidationMessage } from '@/common/enums/message.enum';
+
+class FileChecker
+{
+    private static instance: FileChecker;
+
+    static get(): FileChecker
+    {
+        if (!FileChecker.instance)
+        {
+            FileChecker.instance = new FileChecker();
+        }
+        return FileChecker.instance;
+    }
+
+    CheckImageFormat(file: Express.Multer.File)
+    {
+        const formats = [ '.png', '.jpg', '.jpeg' ];
+        const extension = extname(file.originalname).toLowerCase();
+        if (!formats.includes(extension)) throw new HttpException(ValidationMessage.InvalidImageFormat, HttpStatus.BAD_REQUEST);
+    }
+}
+
+
+const instance = FileChecker.get();
+
+export { instance as FileChecker };
