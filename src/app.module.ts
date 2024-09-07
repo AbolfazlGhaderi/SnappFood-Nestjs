a82f6941e5f1f14cf35b90683996610f9c820662
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-09-07 09:57:05 +0330

    Feat : Add Category Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { DatabaseConfigs } from './configs/database.config';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigs } from './configs/database.config';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ envFilePath: '.env', isGlobal:true }),
         TypeOrmModule.forRoot(DatabaseConfigs()),
+        CategoryModule,
     ],
 })
 export class AppModule {}
