d27f5ec8460747204cb550c5e8abf5f0793e45b2
Abolfazl Ghaderi <brianprogrammer0@gmail.com>
2024-09-07 11:57:03 +0330

    Feat : Add category repository

---
+++ src/modules/category/category.repository.ts
@@ -0,0 +1,22 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from '@/entities/category.entity';
+
+@Injectable()
+export class CategoryRepository extends Repository<CategoryEntity>
+{
+
+    constructor(
+        @InjectRepository(CategoryEntity)
+        private readonly categoryRepository: Repository<CategoryEntity>,
+    )
+    {
+        super(categoryRepository.target, categoryRepository.manager, categoryRepository.queryRunner);
+    }
+
+    async findOneBySlug(slug:string)
+    {
+        return await this.findOne({ where:{ slug:slug } });
+    }
+}
